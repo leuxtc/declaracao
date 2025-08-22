f20d3fc8ef7552b85b27cc435bfd90cfe29101db
leuxtc <l3oxon3@gmail.com>
2025-08-22 08:53:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const element = document.getElementById("txt")
+const nomeC = document.getElementById("nomeC")
+const declarante = document.getElementById("declarante")
+const cpf = document.getElementById("cpf")
+
+function nomeCidadao(nomeC, declarante, cpf, ){
+    
+}
+
+element.innerHTML = `Declaro para os devidos fins, que o(a) Sr(a). ${nomeCidadao(nomeC)} Esteve`
